6624d7e3b4150478a84bb5e4db39e08300e39d69
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-08-12 19:08:41 -0300

    Remove extensoes .js

--- index.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="typings/tsd.d.ts" />
-var angular2_1 = require('angular2/angular2');
-var app_cmp_js_1 = require('app/app_cmp.js');
-angular2_1.bootstrap(app_cmp_js_1.AppCmp)
-    .then(function () { return console.log('index inicializada'); });
