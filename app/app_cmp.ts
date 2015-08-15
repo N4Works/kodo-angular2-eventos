4d2420c7b2c6804724d472d565232bbbe1c2c5bd
Eric Dantas <ericdantas0@gmail.com>
2015-08-15 19:26:52 -0300

    Adiciona exemplo de toggle

--- app/app_cmp.ts
+++ app/app_cmp.ts
@@ -4,7 +4,8 @@ import {Component, View, bootstrap} from 'angular2/angular2';
 import {EventoDeClickSemBubbleCmp} from 'app/evento_de_click_sem_bubble_cmp.js';
 import {EventoDeClickComBubbleCmp} from 'app/evento_de_click_com_bubble_cmp.js';
 import {EventoDeInputComBubbleCmp} from 'app/evento_de_input_com_bubble_diretiva_cmp.js';
-import {EventoCustomizadoCmp} from './evento_customizado_cmp.js';
+import {EventoCustomizadoCmp} from 'app/evento_customizado_cmp.js';
+import {ToggleWrapper} from 'app/evento_click_toggle_cmp.js';
 
 @Component({
     selector: 'app'
@@ -16,8 +17,9 @@ import {EventoCustomizadoCmp} from './evento_customizado_cmp.js';
         <evento-de-click-com-bubble></evento-de-click-com-bubble>
         <evento-de-input-com-bubble></evento-de-input-com-bubble>
         <evento-customizado></evento-customizado>
+        <toggle-wrapper></toggle-wrapper>
     `,
-    directives: [EventoDeClickSemBubbleCmp, EventoDeClickComBubbleCmp, EventoDeInputComBubbleCmp, EventoCustomizadoCmp]
+    directives: [EventoDeClickSemBubbleCmp, EventoDeClickComBubbleCmp, EventoDeInputComBubbleCmp, EventoCustomizadoCmp, ToggleWrapper]
 })
 
 export class AppCmp {
@@ -29,6 +31,7 @@ export class AppCmp {
 Promise.all([ bootstrap(EventoDeClickSemBubbleCmp),
               bootstrap(EventoDeClickComBubbleCmp),
               bootstrap(EventoDeInputComBubbleCmp),
-              bootstrap(EventoCustomizadoCmp) ])
+              bootstrap(EventoCustomizadoCmp),
+              bootstrap(ToggleWrapper) ])
        .then(() => console.log('bootstrap da app feita corretamente'))
        .catch((erro) => console.log(`erro durante bootstrap da app: ${erro}`));
